a4429f8b32b564b4d4b177bc658eab89e78615ef
fouadabiad <abiadfm@gmail.com>
2025-02-26 23:56:34 +0100

    implementing fragile watermark + spotted difference between different functions

--- ECG_fragile.py
+++ ECG_fragile.py
@@ -14,17 +14,61 @@ ecg_signal = 1.2 * np.sin(2 * np.pi * 1 * x_values) + \
              2.5 * np.exp(-((x_values - 0.37)/0.018)**2) + \
              -1.2* np.exp(-((x_values - 0.42)/0.025)**2) + \
              0.6 * np.exp(-((x_values - 0.65)/0.04)**2)
- 
-ecg_bytes     = ecg_signal.tobytes()
-watermark_hex = hashlib.sha256(ecg_bytes).hexdigest()
-watermark_bin = bin(int(watermark_hex, 16))[2:].zfill(256)
 
-ecg_int = np.array(ecg_signal * 1000, dtype=np.int32) # Scale + convert to int
-for i in range(len(watermark_bin)):
-    ecg_int[i] = (ecg_int[i] & ~1) | int(watermark_bin[i])
-watermarked_ecg = ecg_int/1000
+SHA256_OUTPUT_LEN = 256
+HEX_BASE          = 16
+ECG_SCALE_FACTOR  = 1000
 
-print(type(~5))
+def convert_ecg_signal_to_binary(ecg_signal: np.array) -> str:
+    """Converts ECG signal > bite array > SHA256 hash > binary watermark
+    Note the lengths: sha256 hash (256bit) = 64char, binary watermark = 256char"""
 
+    ecg_in_bytes  = ecg_signal.tobytes()
+    sha256_hash   = hashlib.sha256(ecg_in_bytes).hexdigest() # len = 64
+    watermark_bin = bin(int(sha256_hash, HEX_BASE))[2:].zfill(SHA256_OUTPUT_LEN)
+    return watermark_bin
+
+def embed_watermark_in_ecg(ecg_signal: np.array, watermark_bin: str) -> np.array:
+    """Embeds binary watermark in ECG signal, by replacing the least significant 
+    bit with the watermark bit of each item. Uses a scaling factor to reduce
+    the watermark's effect on the signal"""
+
+    ecg_scaled_int = np.array(ecg_signal*ECG_SCALE_FACTOR, dtype=np.int32) # Scale + convert to int
+    for i in range(len(watermark_bin)):
+        ecg_scaled_int[i] = (ecg_scaled_int[i] & ~1) | int(watermark_bin[i])
+        print(ecg_scaled_int[i]/ECG_SCALE_FACTOR- ecg_signal[i])
+    watermarked_ecg = ecg_scaled_int/ECG_SCALE_FACTOR
+    # print(watermarked_ecg - ecg_signal)
+    return watermarked_ecg
+
+
+def embed_watermark_my_way(ecg_signal: np.array, watermark_bin: str) -> np.array:
+    # what to do with -ve values? the binary becomes -ve too
+
+    # seems the issue is with the sign, when subtracting original-modified
+    # i get -1, 0, 1. not sure how to tackle it, but its definitely a problem
+    # look into how sign is handled in paper, and in binary code
+    # CONFIRMED: the other function does not have the subtracted value as -1,1,0, but some
+    # decimals (not sure how is that possible)
+
+    ecg_scaled_int = np.array(ecg_signal*ECG_SCALE_FACTOR, dtype=np.int32) # Scale + convert to int
+    modified_ecg_signal = np.copy(ecg_scaled_int)
+    for i, bit in enumerate(watermark_bin):
+        ecg_binary        = list(bin(ecg_scaled_int[i]))
+        ecg_binary[-1]    = bit
+        modified_ecg_component = int("".join(ecg_binary), 2)
+        # print(ecg_scaled_int[i]- modified_ecg_component)
+        modified_ecg_signal[i] = modified_ecg_component
+
+    watermarked_ecg = modified_ecg_signal/ECG_SCALE_FACTOR
+    return watermarked_ecg
+
+
+watermark_bin   = convert_ecg_signal_to_binary(ecg_signal)
+watermarked_ecg = embed_watermark_in_ecg(ecg_signal, watermark_bin)
+watermarked_ecg2= embed_watermark_my_way(ecg_signal, watermark_bin)
+
+print(np.mean(np.abs(watermarked_ecg - ecg_signal)/ecg_signal) * 100)
+print(np.mean(np.abs(watermarked_ecg2 - ecg_signal)/ecg_signal) * 100)
 
 
