0a57efd7060987f7e433f90838d3f9aa81fdaf22
abiadf <abiadfm@gmail.com>
2025-02-26 17:55:14 +0100

    edit to fragile watermark

--- ECG_fragile.py
+++ ECG_fragile.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import hashlib
- 
+
 n_timesteps= 400 # from me
 fs         = 20 # sampling freq, from me (make sure its >2x the highest dominant signal freq)
 x_values   = np.arange(0, n_timesteps/fs, 1/fs)
@@ -15,9 +15,16 @@ ecg_signal = 1.2 * np.sin(2 * np.pi * 1 * x_values) + \
              -1.2* np.exp(-((x_values - 0.42)/0.025)**2) + \
              0.6 * np.exp(-((x_values - 0.65)/0.04)**2)
  
-ecg_bytes = ecg_signal.tobytes()
-watermark = hashlib.sha256(ecg_bytes).hexdigest()
-watermark_bin = bin(int(watermark, 16))[2:].zfill(256)
- 
- 
-print(watermark_bin)
+ecg_bytes     = ecg_signal.tobytes()
+watermark_hex = hashlib.sha256(ecg_bytes).hexdigest()
+watermark_bin = bin(int(watermark_hex, 16))[2:].zfill(256)
+
+ecg_int = np.array(ecg_signal * 1000, dtype=np.int32) # Scale + convert to int
+for i in range(len(watermark_bin)):
+    ecg_int[i] = (ecg_int[i] & ~1) | int(watermark_bin[i])
+watermarked_ecg = ecg_int/1000
+
+print(type(~5))
+
+
+
